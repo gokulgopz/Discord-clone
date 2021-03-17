c021f74005287ab635fdab29b29de87b32db99d9
Gokul_Gopz <51716163+gokulgopz@users.noreply.github.com>
2021-03-17 10:18:58 +0530

    Add files via upload

---
+++ firebase.js
@@ -0,0 +1,10 @@
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+    apiKey: "AIzaSyDd8GUyoJ0G9tMyaUXv7vt11jzNEiP9i2c",
+    authDomain: "discord-clone-123e3.firebaseapp.com",
+    projectId: "discord-clone-123e3",
+    storageBucket: "discord-clone-123e3.appspot.com",
+    messagingSenderId: "832201650246",
+    appId: "1:832201650246:web:c948292b3438c0a64bda47",
+    measurementId: "G-SR92Y16CDM"
+  };
